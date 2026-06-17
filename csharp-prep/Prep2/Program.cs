e41e0038ad93f95dc0ff914bfb14aa825ca2d1ec
IzakB42 <bea19016@byui.edu>
2026-06-17 13:12:11 -0600

    Prep 2, grade core if statements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        float grade = float.Parse(Console.ReadLine());
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";    
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";    
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";    
+        }      
+        else if (grade < 60)
+        {
+            letter = "F";    
+        }  
+
+        Console.WriteLine($"Your letter grade is {letter}"); 
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry! Better luck next time!");
+        }
+
     }
 }
